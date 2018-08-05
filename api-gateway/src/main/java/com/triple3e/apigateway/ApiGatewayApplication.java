411f640d0e598b7cda25f27f5dee1e227296e4af
ppsseo18 <ppsseo18@gmail.com>
2018-08-05 20:21:03 +0900

    zuul 수정

--- api-gateway/src/main/java/com/triple3e/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/triple3e/apigateway/ApiGatewayApplication.java
@@ -3,8 +3,9 @@ package com.triple3e.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZ
+@EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayApplication {
