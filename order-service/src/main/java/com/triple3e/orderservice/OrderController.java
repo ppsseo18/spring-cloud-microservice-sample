ff9f3530c56bb3f5e095bc76dd3ff097cf449c77
ppsseo18 <ppsseo18@gmail.com>
2018-09-14 00:44:11 +0900

    order service 수정

--- order-service/src/main/java/com/triple3e/orderservice/OrderController.java
+++ order-service/src/main/java/com/triple3e/orderservice/OrderController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
@@ -19,10 +20,18 @@ public class OrderController {
     @Autowired
     OrderRepository orderRepository;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/send")
     public String sendOrder() {
         eventPublisher.publish("send order!");
         return "success";
+    }
+
+    @GetMapping("/users")
+    public String getUser() {
+        return restTemplate.getForObject("http://user-service/", String.class);
     }
 
     @GetMapping("/products/{id}")
